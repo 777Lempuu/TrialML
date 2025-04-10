921d380d81f42e5d07404d753cb90e16249d2f4a
777Lempuu <131601272+777Lempuu@users.noreply.github.com>
2025-04-11 00:54:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("AG News Dataset (Google Drive Hosted)")
 
 @st.cache_data
 def load_agnews_csv():
-    url = "https://drive.google.com/uc?id=1AbCDefgHiJKlMnO1234567890abcdef"  # ← Replace with your real ID
+    url = "https://drive.google.com/file/d/1xr-eyagU6GeZlYpn8qGIuMSdK5WFUV5x/view?usp=drive_link"  # ← Replace with your real ID
     return pd.read_csv(url)
 
 df = load_agnews_csv()
